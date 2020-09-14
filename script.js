bdffc4a6c55fce11398d240ba0676597f1f9f520
kburgosdavila <71047387+kburgosdavila@users.noreply.github.com>
2020-09-13 23:55:33 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var firstName; 
+firstName = "Kali";
+var lastName;
+lastName = "Burgos";
+var Age = 23;
+
+alert("My name is Kali Burgos");
+alert("I am 23 years old");
+alert("I am legally able to drink in the states")
+
+console.log("I am having so much fun in this class")
